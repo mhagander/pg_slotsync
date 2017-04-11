81f556c2752d8e1589f424c527f0c8a3fda8f819
Magnus Hagander <magnus@hagander.net>
2017-04-11 14:19:27 +0200

    Should not be public executable by default

--- pg_slotcontrol/pg_slotcontrol--1.0.sql
+++ pg_slotcontrol/pg_slotcontrol--1.0.sql
@@ -5,3 +5,5 @@ CREATE FUNCTION pg_slotmove(text, pg_lsn)
        RETURNS boolean
        AS 'MODULE_PATHNAME', 'pg_slotmove'
        LANGUAGE C VOLATILE STRICT;
+
+REVOKE ALL ON FUNCTION pg_slotmove(text, pg_lsn) FROM PUBLIC;
